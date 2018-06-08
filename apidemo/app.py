25b0871a71a28515156e176d001aa4c1ae74d2a1
Dave Chen <totorochina@hotmail.com>
2018-06-08 17:17:48 +0800

    first commit

---
+++ apidemo/app.py
@@ -0,0 +1,30 @@
+import json
+
+import requests
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Arguments:
+        event LambdaEvent -- Lambda Event received from Invoke API
+        context LambdaContext -- Lambda Context runtime methods and attributes
+
+    Returns:
+        dict -- {'statusCode': int, 'body': dict}
+    """
+
+    if event['path'] == '/':
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                'message': '1.1.1.1',
+            })
+        }
+    else:
+        ip = requests.get('http://checkip.amazonaws.com/')
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                'message': ip.text.replace('\n', ''),
+            })
+        }
