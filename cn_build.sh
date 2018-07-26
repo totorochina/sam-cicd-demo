721c82033fb2a3c7f6c69c044d50e8ad830e2963
Dave Chen <totorochina@hotmail.com>
2018-07-26 10:16:57 +0800

    updated .sh for China regions

---
+++ cn_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME="hzchen-private"
+REGION="cn-north-1"
+STACK="sam-apidemo-prod"
+
+pip install -r requirements.txt -t apidemo/build/ && \
+cp apidemo/*.py apidemo/build/ && \
+
+sam validate && \
+sam package \
+	    --template-file cn_template.yaml \
+	        --output-template-file cn_packaged.yaml \
+		    --s3-bucket $REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME \
+		    --region $REGION
---
+++ cn_build_and_deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME="hzchen-private"
+REGION="cn-north-1"
+STACK="sam-apidemo-prod"
+
+pip install -r requirements.txt -t apidemo/build/ && \
+cp apidemo/*.py apidemo/build/ && \
+
+sam validate && \
+sam package \
+	    --template-file cn_template.yaml \
+	        --output-template-file cn_packaged.yaml \
+		    --s3-bucket $REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME \
+		    --region $REGION && \
+
+sam deploy \
+	    --template-file cn_packaged.yaml \
+	        --stack-name $STACK \
+		    --capabilities CAPABILITY_IAM \
+		        --parameter-overrides MyParameterSample=MySampleValue \
+			--region $REGION && \
+
+aws cloudformation describe-stacks \
+	    --stack-name $STACK --query 'Stacks[].Outputs' \
+	    --region $REGION
